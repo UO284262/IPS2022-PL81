1c0cf11da6b73d104e5f455aa87408244400264c
Abel <Abel@LAPTOP-NTHSQBC1>
2022-10-12 20:34:40 +0200

    inicio HU 2 abel (definicion de querys y estructura)

--- src/abel/modelo/DataBaseManagement.java
+++ src/abel/modelo/DataBaseManagement.java
@@ -3,7 +3,10 @@ package abel.modelo;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.sql.Date;
 
 import main.DatabaseConnection;
@@ -13,6 +16,10 @@ public class DataBaseManagement
 	
 	private final static String QUERY_INSERT_ACTIVIDAD_FORMATIVA = "INSERT INTO Actividad_Formativa(nombre,precio) VALUES(\"%s\",%s);";
 	private final static String QUERY_INSERT_FECHA_IMPARTICION  = "INSERT INTO Fecha_Imparticion VALUES(\"%s\",'%s');";
+	private final static String QUERY_OBTENER_ACTIVIDADES_FORMATIVAS = "SELECT DISTINCT nombre FROM Actividad_Formativa NATURAL JOIN"
+																	+ "Fecha_Imparticion WHERE fecha >= \"%s\";";
+	private final static String QUERY_OBTENER_INSCRITOS_ACTIVIDAD_FORMATIVA = "SELECT c.nombre, c.apellidos, a.fecha_inscripcion FROM apuntado a NATURAL JOIN"
+																			+ "Colegiado c WHERE a.nombre_curso = \"%s\";";
 	
 	public static boolean addActividadToDataBase(ActividadFormativaDTO actividad)
 	{
@@ -31,5 +38,32 @@ public class DataBaseManagement
 			return false;
 		}
 		return true;
+	}
+	
+	public static List<String> getActividadesFormativasFrom(Integer año)
+	{
+		try (Connection conn = DatabaseConnection.getConnection();)
+		{	
+			conn.setAutoCommit(false);
+			PreparedStatement st = conn.prepareStatement(String.format(QUERY_OBTENER_ACTIVIDADES_FORMATIVAS,año.toString() + "-1-1"));
+			ResultSet rs = st.executeQuery();
+			conn.commit();
+			List<String> nombres = toNombreList(rs);
+			st.close();
+			rs.close();
+			return nombres;
+			
+		} catch (SQLException e) { }
+		return null;
+	}
+	
+	private static List<String> toNombreList(ResultSet rs) throws SQLException
+	{
+		List<String> nombres = new ArrayList<String>();
+		while(rs.next())
+		{
+			nombres.add(rs.getString("nombre"));
+		}
+		return nombres;
 	}
 }
---
+++ src/abel/vista/VisualizarInscritosCurso.java
@@ -0,0 +1,42 @@
+package abel.vista;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class VisualizarInscritosCurso extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VisualizarInscritosCurso frame = new VisualizarInscritosCurso();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VisualizarInscritosCurso() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
